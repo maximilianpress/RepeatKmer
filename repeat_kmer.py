45b5bcb31f57dab5b8b47f3bf0704262ac8b4fed
Maximilian Press <maximilianpress5@gmail.com>
2020-08-08 10:28:11 -0700

    Moved repeat_kmer.py to main dir.

--- repeat_kmer.py
+++ repeat_kmer.py
@@ -5,18 +5,17 @@ maximilian press
 
 RepeatKmer/RepeatKmer/repeat_kmer.py
 
-Use a simplistic k-mer tree strategy with a greedy pruning heuristic to 
+Use a simplistic k-mer tree strategy with a greedy pruning heuristic to
 find overrepresented (repetitive) sequences in a genome.
 
 """
-from __future__ import print_function
 import RepeatKmer.kmer_utils as ku
 from RepeatKmer.kmer_tree import KmerTree
 
 def main():
     c_args = ku.parse_args()
     tree = KmerTree(root_k=c_args["root_k"], genome_file=c_args["genome_file"],
-                    out_prefix=c_args["out_prefix"])
+                    out_prefix=c_args["out_file_prefix"])
     tree.grow_the_tree()
     tree.select_maximal_repeats()
     tree.yield_maximal_repeats()
